729c62ceff3e742a423c0f9a8d460081a6589204
Rogerio Libarino Aguilar <rogerio.libarino@nubank.com.br>
2023-04-18 02:43:25 -0300

    Remove index route for Client and Invoice

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :invoices
-  resources :clients, param: :external_id
+  resources :invoices, only: [:show, :create, :update, :destroy]
+  resources :clients, param: :external_id, only: [:show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
